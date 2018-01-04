e56f66392d77af2eb3320fe39458fdb67b9e27b7
Justin Escabarte <maximusarellius0923@gmail.com>
2018-01-04 21:35:54 +0800

    added files

---
+++ The_Count_method_on_a_String.rb
@@ -0,0 +1,34 @@
+puts "Hello World".count("lo") # outs 5 because 3 l's and 2 o's
+
+
+def custom_count(string, char)
+current = 0
+string.chars do |i|
+if i == char
+  current += 1
+end
+end
+p current
+end
+
+def custom_count_simplified(string, char)
+  current = 0
+  string.chars { |i| current += 1 if i == char}
+  p current
+end
+
+
+
+def boris_way(string, char)
+  count = 0
+  string.each_char do |i|
+  if i.include?(char)
+    count += 1
+  end
+end
+p count
+end
+
+custom_count("Hello world", "l")
+custom_count_simplified("Hello world", "l")
+boris_way("hello world","l")
