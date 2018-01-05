664e257f6456f75cdf5a2c12a84e943b6331338c
Justin Escabarte <maximusarellius0923@gmail.com>
2018-01-06 00:39:33 +0800

    added files

---
+++ The_clear_Method_on_a_String.rb
@@ -0,0 +1,5 @@
+word = "goodbye"
+
+word.clear
+
+p word
---
+++ The_delete_method.rb
@@ -0,0 +1,29 @@
+puts "Hello World".delete("o") #delete all o's
+
+
+def custom_delete(string, char)
+word = []
+string.chars do |i|
+  word.push(i) unless i == char
+
+
+
+end
+puts word.join
+end
+
+def boris_way(string, deleted_characters)
+  new_string = ""
+  string.each_char do |char|
+    unless deleted_characters.include?(char)
+      new_string << char
+    end
+  end
+    puts new_string
+  end
+
+
+
+boris_way("Hello World", "l")
+custom_delete("Hello World","orld")
+p custom_delete("Hello World","l") == boris_way("Hello World", "l")
