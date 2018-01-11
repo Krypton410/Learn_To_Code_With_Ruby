e76463e6cc9f744680e3dfd215193d116e497784
Justin Escabarte <maximusarellius0923@gmail.com>
2018-01-11 23:59:16 +0800

    added files

---
+++ Review_of_the_class_Method_on_Ruby_Objects.rb
@@ -0,0 +1,13 @@
+puts 5.class
+puts 2.33.class
+puts false.class
+puts -2.class
+puts [].class
+puts 0.93 == 4.34
+puts "c".class
+puts Hash.new(0).class
+puts (0..9).class
+puts :sd.class
+puts lambda{}.class
+puts Time.new.class
+puts //.class.superclass
---
+++ The_superclass_and_ancestor_Methods_on_a_Class.rb
@@ -0,0 +1,7 @@
+p 5.class.superclass.superclass.superclass
+p 2.14.class.superclass.superclass.superclass
+
+p 5.class.ancestors
+p 23.23.class.ancestors
+
+p Hash.new(0).class.superclass.superclass == Hash.new(0).class.superclass.superclass
